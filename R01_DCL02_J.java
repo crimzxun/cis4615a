620e0f6d56ce5af024378b1d5d86f25c641dc4c5
Anna Zheng <anna.zheng@ucf.edu>
2023-10-08 04:30:41 -0400

    process the modified list but leaves the actual list unchanged

--- R01_DCL02_J.java
+++ R01_DCL02_J.java
@@ -5,16 +5,18 @@ List<Integer> list = Arrays.asList(new Integer[] {13, 14, 15});
 boolean first = true;
  
 System.out.println("Processing list...");
-for (Integer i: list) {
-  if (first) {
-    first = false;
-    i = new Integer(99);
-  }
-  System.out.println(" New item: " + i);
-  // Process i
+for (final Integer i: list) 
+{
+	Integer item = i;
+	if (first) {
+	first = false;
+	i = new Integer(99);
+	}
+	System.out.println(" New item: " + i);
+	// Process i
 }
  
 System.out.println("Modified list?");
 for (Integer i: list) {
-  System.out.println("List item: " + i);
+	System.out.println("List item: " + i);
 }
