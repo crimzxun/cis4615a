01aa498bc25a4a7495faeeba711be6988baeb849
Anna Zheng <anna.zheng@ucf.edu>
2023-10-08 08:17:46 -0400

    never synchronize on objects that may be reused

--- R09_LCK01_J.java
+++ R09_LCK01_J.java
@@ -2,7 +2,7 @@
 // LCK01-J. Do not synchronize on objects that may be reused
 
 private int count = 0;
-private final Integer Lock = count; // Boxed primitive Lock is shared
+private final Integer Lock = new Integer(count);
  
 public void doSomething() 
 {
