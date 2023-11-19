25778006085d805b10e939dc25370f59b05a10a4
Anna Zheng <anna_zheng@knights.ucf.edu>
2023-11-19 03:15:37 -0500

    Update cis4615a.java

--- cis4615a.java
+++ cis4615a.java
@@ -13,7 +13,7 @@
  *
  ******************************************************************************/
 
-public class Cis4615a {
+public class cis4615a {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
